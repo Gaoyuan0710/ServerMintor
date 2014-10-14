da23aeb45e5b1b2242a251d68cf7bd780603ef43
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-14 22:00:14 +0800

    add file socketOperator.*

---
+++ client/include/socketOperator.h
@@ -0,0 +1,54 @@
+// =====================================================================================
+// 
+//       Filename:  socketOperator.cpp
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2014年10月14日 21时26分14秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+//        Company:  Class 1204 of Computer Science and Technology
+// 
+// =====================================================================================
+#ifndef _SOCKETOPERATOR_H_
+#define _SOCKETOPERATOR_H_
+
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define BUFLINE 1024
+
+namespace mySystemMintor{
+
+
+struct packet{
+	int types;
+	int len;
+	char value[BUFLINE];
+};
+
+class sockOperator{
+	public:
+		sockOperator(){}
+		bool setAddr(string des, string port);
+	private:
+		struct sockaddr_in destAddress;
+	
+
+
+}
+
+
+}
---
+++ client/src/socketOperator.cpp
@@ -0,0 +1,24 @@
+// =====================================================================================
+// 
+//       Filename:  socketOperator.cpp
+//
+//    Description:  
+//
+//        Version:  1.0
+//        Created:  2014年10月14日 21时26分14秒
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  Gaoyuan, sishuiliunian0710@gmail.com
+//        Company:  Class 1204 of Computer Science and Technology
+// 
+// =====================================================================================
+
+#include <iostream>
+
+#include "socketOperator.h"
+
+namespace mySystemMintor{
+bool sockOperator:: 
+}
+
