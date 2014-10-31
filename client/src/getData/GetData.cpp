76b78f9a25aad0727fe3ddb1197daca4768d7d17
Gaoyuan0710 <sishuiliunian0710@gmail.com>
2014-10-31 15:46:23 +0800

    修改计算CPU占有率公式,测试正确w

--- client/src/getData/GetData.cpp
+++ client/src/getData/GetData.cpp
@@ -98,87 +98,51 @@ string GetData::getCpuRate(){
 	char cpu[5];
 	char tmp[256];
 	string Info = "";
-	long int user[5], nice[5], sys[5], idle[5], iowait[5], irq[5], softirq[5];
-	long int all1[5], all2[5], idle1[5], idle2[5];
+	long  user[5], nice[5], sys[5], idle[5], iowait[5], irq[5], softirq[5];
+	long  all1[5], all2[5];
 	float usage[5];
-
-	long int user_pass[5], sys_pass[5], idle_pass[5];
-
-	int j = 5;
-	while (j--){
-		fp = fopen("/proc/stat", "r");
+	long int tt1[5], tt2[5];
+	int j = 2;
 	
-		if (fp == NULL){
-			Info.append("Info Get Error");
-
-			return Info;
-		}
-		int i = 0;
-		while (i < 5){
-			fgets(buf, sizeof(buf), fp);
-			sscanf(buf, "%s%d%d%d%d%d%d%d", cpu, &user[i], &nice[i], &sys[i],
-						&idle[i], &iowait[i], &irq[i], &softirq[i]);
-			
-			all1[i] = user[i] + nice[i] + sys[i] + idle[i] + iowait[i] + irq[i] + softirq[i];
-			idle1[i] = idle[i];
-			user_pass[i] = user[i] + sys[i];
-			i++;
-		}
-		rewind(fp);
-		sleep(WAIT_TIME);
-		memset(buf, 0, sizeof(buf));
-	
-		memset(user, 0, sizeof(user));
-		memset(nice, 0, sizeof(nice));
-		memset(sys, 0, sizeof(sys));
-		memset(idle, 0, sizeof(idle));
-		memset(irq, 0, sizeof(irq));
-		memset(softirq, 0, sizeof(softirq));
-		memset(iowait, 0, sizeof(iowait));
-	
-	
-	//	user = nice = sys = idle = iowait = irq = softirq = 0;
-
-		i = 0;
-
-		while (i < 5){
-			fgets(buf, sizeof(buf), fp);
-			sscanf(buf, "%s%d%d%d%d%d%d%d", cpu, &user[i], &nice[i], &sys[i],
-						&idle[i], &iowait[i], &irq[i], &softirq[i]);
-			
-			all2[i] = user[i] + nice[i] + sys[i] + idle[i] + iowait[i] + irq[i] + softirq[i];
-			idle2[i] = idle[i];
-			sys_pass[i] = user[i] + sys[i];
-			i++;
-		}
-		i = 0;
-		while (i < 5){
-			usage[i] = ((float) (user_pass[i] - sys_pass[i]) )/ (user_pass[i] - sys_pass[i] - idle2[i] + idle1[i]) * 100;
-
-		//	usage[i] = ((float) (user2[i] + sys2[i] - user1[i] - sys ) )/ (user[i] + nice[i] + sys[i] + idle[i])  * 100;
-			cout << i << " : " << usage[i] << endl;
-			i++;
-		}
-		i = 0;
-
-	/*	while (i < 5){
-			if (i == 0){
-				cout << "Cpu Use " << usage[i] << endl;
-				i++;
-				continue;
-			}
-	
-			cout << "Cpu" << i << " Use " << usage[i] << endl;
-
-		//		Info.append("Cpu Use ");
-	//		sprintf(tmp, "%.2f", usage);
-	//		Info.append(tmp);
-			i++;
-		}
-		j--;*/
-	//	close(fp);
+	fp = fopen("/proc/stat", "r");
+	if (fp == NULL){
+		Info.append("Info Get Error");
+		return Info;
 	}
+	int i = 0;
+	while (i < 5){
+		fgets(buf, sizeof(buf), fp);
+		sscanf(buf, "%s %ld %ld %ld %ld %ld %ld %ld", cpu, &user[i], &nice[i], 
+					&sys[i], &idle[i], &iowait[i], &irq[i], &softirq[i]);
+		all1[i] = user[i] + nice[i] + sys[i] + idle[i] + iowait[i] + irq[i] + softirq[i];
+		tt1[i] = user[i] + sys[i];
+		i++;
+	}
+	sleep(2);
+	fclose(fp);
+	
+	fp = fopen("/proc/stat", "r");
+	rewind(fp);
+	i = 0;
+	while (i < 5){
+		fgets(buf, sizeof(buf), fp);
+		sscanf(buf, "%s %ld %ld %ld %ld %ld %ld %ld", cpu, &user[i], &nice[i], &sys[i], 
+					&idle[i], &iowait[i], &irq[i], &softirq[i]);
+		all2[i] = user[i] + nice[i] + sys[i] + idle[i] + iowait[i] + irq[i] + softirq[i];
+		tt2[i] = user[i] + sys[i];
+		usage[i] = (((float) (tt2[i] - tt1[i]) )/ (all2[i] - all1[i])) * 100;
+		i++;
+	}
+	i = 0;
+	while (i < 5){
+		cout << "Cpu" << i << " Use " << usage[i] << endl;
+		Info.append("Cpu Use ");
+		sprintf(tmp, "%.2f", usage[i]);
+		Info.append(tmp);
+		Info.append("\n");
+		i++;
+	}
+	fclose(fp);
 	return Info;
-
 }
 
